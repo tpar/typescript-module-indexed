92e8c3119799c015cfed97e559ad64822c9e62f8
Tejas Parnerkar <tejas@Tejass-MacBook-Air.local>
2018-10-06 16:34:46 +1300

    Update the name of the command, promput for user input

---
+++ src/commands/commands.ts
@@ -0,0 +1,5 @@
+
+
+export namespace CommmandIds {
+    export const NEW_TS_MODULE_COMMAND_ID = 'New.Ts.Module';
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,27 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+interface NewModuleParams {
+    fsPath: string;
+    scheme: string;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "typescript-module-indexed" is now active!');
+    let disposable = vscode.commands.registerCommand('New.Ts.Module', (args: NewModuleParams) => {
+        
+        const a = args;
+        let input = '';
+        vscode.window.showInputBox({
+            placeHolder: 'Please enter the module name',
+            prompt: 'Enter a hyphen seperated module name, e.g. new-ts-module',
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+        }).then(val => {
+            input = val as string || '';
+            console.debug(input);
+        });
 
-        // Display a message box to the user
-        //vscode.window.showInformationMessage('Hello World!');
-
+       
 
     });
 
