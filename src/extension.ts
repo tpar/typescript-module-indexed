d00ce8eafaf837cb7f92284cbbe322c7efa0546f
Tejas Parnerkar <tejas@Tejass-MacBook-Air.local>
2018-10-07 11:44:35 +1300

    create a new dir based on module name

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,40 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 interface NewModuleParams {
-    fsPath: string;
-    scheme: string;
+	fsPath: string;
+	scheme: string;
 }
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let disposable = vscode.commands.registerCommand('New.Ts.Module', (args: NewModuleParams) => {
-        
-        const a = args;
-        let input = '';
-        vscode.window.showInputBox({
-            placeHolder: 'Please enter the module name',
-            prompt: 'Enter a hyphen seperated module name, e.g. new-ts-module',
+	let disposable = vscode.commands.registerCommand('New.Ts.Module', async (args: NewModuleParams) => {
+	
+		try {
+			const moduleName = await vscode.window.showInputBox({
+				placeHolder: 'Please enter the module name',
+				prompt: 'Enter a hyphen seperated module name, e.g. new-ts-module',
+			});
 
-        }).then(val => {
-            input = val as string || '';
-            console.debug(input);
-        });
+			if (moduleName) {
+				const commands = await vscode.commands.getCommands();
+				console.log(commands); 
+				fs.mkdirSync(args.fsPath + '/' + moduleName);
+			}
+		}
 
-       
+		catch(ex) {
+			console.error(ex);
+		}
 
-    });
+		
+
+	});
 
 
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
